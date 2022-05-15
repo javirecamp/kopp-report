1657c36a8a984dc4ae1c0a682d6b627726433a5a
Choi DeokJae <88370895+cdj6995@users.noreply.github.com>
2022-05-15 22:05:38 +0900

    최선을 다했습니다

---
+++ 20220513-3DAY/최덕재/homepage.js
@@ -0,0 +1,13 @@
+// 토글 버튼 객체를 가져오는 녀석(객체)
+const togleBtn = document.querySelector(".navbar_togleBtn");
+// 메뉴 버튼 객체를 가져오는 녀석(객체)
+const menu = document.querySelector(".navbar_menu");
+// 아이콘 버튼 객체를 가져오는 녀석(객체)
+const icons = document.querySelector(".navbar_icons");
+
+// 이벤트를 사용하려면 객체를 만들고 이벤트 리스너를 붙여야 사용이 가능
+togleBtn.addEventListener("click", ()=>{
+    menu.classList.toggle('active');
+    icons.classList.toggle('active');
+    
+})
