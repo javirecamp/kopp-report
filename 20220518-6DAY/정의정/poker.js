3662aad295fdd40bc799846f978b8e3eeefdcb99
uijeongg <100887436+uijeongg@users.noreply.github.com>
2022-05-19 08:49:12 +0900

    Update poker.js

--- 20220518-6DAY/정의정/poker.js
+++ 20220518-6DAY/정의정/poker.js
@@ -3,27 +3,30 @@ document.addEventListener('DOMContentLoaded', ()=>{
    
     let doc=document.querySelector("#start");
 
-    const poker = []; //포커배열생성
-    const num = ["A","2","3","4","5","6","7","8","9","T","J","Q","K"];
-    const shape = ["spade","heart","diamond","clova"];
+    const poker = []; //포커배열 생성
+    const num = ["A","2","3","4","5","6","7","8","9","T","J","Q","K"]; //숫자배열 생성
+    const shape = ["spade","heart","diamond","clova"]; //모양배열 생성
 
+    //num배열의 길이만큼 for문을 돎
     for(let i=0; i<num.length; i++) {
 
+        //shape배열의 길이만큼 for문을 돌며 num배열과 shape배열을 합쳐줌
         for(let j=0; j<shape.length; j++) {
-            let pokerCard = num[i]+shape[j]; //i와j를 붙여주기 문자+문자라서 그냥 +로 붙음
-            //얘를 push
-            poker.push(pokerCard);
+            let pokerCard = num[i]+shape[j]; //i와j를 붙여주고 새 배열에 저장
+                                             //문자+문자라서 그냥 +로 붙음
+            
+            poker.push(pokerCard); //얘를 push
 
         }
     }
     
 
-    console.log(poker.length);
+    console.log(poker.length); //호출
     
     
     // 무작위로 배열을 섞어주는 함수
         function shuffle(x) {
-            x.sort(() => Math.random() - 0.5);
+            x.sort(() => Math.random() - 0.5); //정렬 시켜주고 정수화를 위해 -0.5
         }
         
         shuffle(poker);
