e04d35e8d8641a12449e6c69e8822fb6c77116fd
skim24245 <100885920+skim24245@users.noreply.github.com>
2022-05-24 20:42:14 +0900

    Delete 20220524-10DAY/김선영/과제 directory

--- 20220524-10DAY/김선영/과제/js/app.js
+++
@@ -1,7 +0,0 @@
-//MainController와 연결을 한다.
-import MainController from "./controllers/MainController.js"; 
-
-document.addEventListener("DOMContentLoaded",() =>{
-// console.log("app.js");
-    MainController.init();
-});
